030c56c455b5ae680522c5ab45ef6f3c766c56e8
JuricaAleksandar <jurica.aleksandar@gmail.com>
2017-03-26 17:35:34 +0000

    Dugmici reseni

--- projekat/app/src/main/java/ra47_2014/pnrs1/rtrk/taskmanager/AddActivity.java
+++ projekat/app/src/main/java/ra47_2014/pnrs1/rtrk/taskmanager/AddActivity.java
@@ -1,10 +1,15 @@
 package ra47_2014.pnrs1.rtrk.taskmanager;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import java.util.Calendar;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -18,6 +23,11 @@ public class AddActivity extends AppCompatActivity {
         final Button greenButton = (Button) findViewById(R.id.buttonGreen);
         final Button yellowButton = (Button) findViewById(R.id.buttonYellow);
         final Button redButton = (Button) findViewById(R.id.buttonRed);
+
+        greenButton.setBackgroundColor(Color.rgb(30,154,30));
+        redButton.setBackgroundColor(Color.rgb(204,0,0));
+        yellowButton.setBackgroundColor(Color.rgb(255,194,0));
+
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,22 +43,34 @@ public class AddActivity extends AppCompatActivity {
         greenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                yellowButton.setEnabled(false);
-                redButton.setEnabled(false);
+                greenButton.setBackgroundColor(Color.rgb(170,255,170));
+                redButton.setBackgroundColor(Color.rgb(204,0,0));
+                yellowButton.setBackgroundColor(Color.rgb(255,194,0));
+                greenButton.setEnabled(false);
+                redButton.setEnabled(true);
+                yellowButton.setEnabled(true);
             }
         });
         yellowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                greenButton.setEnabled(false);
-                redButton.setEnabled(false);
+                greenButton.setBackgroundColor(Color.rgb(30,154,30));
+                redButton.setBackgroundColor(Color.rgb(204,0,0));
+                yellowButton.setBackgroundColor(Color.rgb(255,224,130));
+                greenButton.setEnabled(true);
+                redButton.setEnabled(true);
+                yellowButton.setEnabled(false);
             }
         });
         redButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                yellowButton.setEnabled(false);
-                greenButton.setEnabled(false);
+                greenButton.setBackgroundColor(Color.rgb(30,154,30));
+                redButton.setBackgroundColor(Color.rgb(255,170,170));
+                yellowButton.setBackgroundColor(Color.rgb(255,194,0));
+                greenButton.setEnabled(true);
+                redButton.setEnabled(false);
+                yellowButton.setEnabled(true);
             }
         });
     }
