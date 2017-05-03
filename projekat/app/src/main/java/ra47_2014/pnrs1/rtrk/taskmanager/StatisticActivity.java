01004ed05e4221fb4dda4635e2fb6594258fe647
JuricaAleksandar <jurica.aleksandar@gmail.com>
2017-05-04 01:19:45 +0200

    zad3 gotov

--- projekat/app/src/main/java/ra47_2014/pnrs1/rtrk/taskmanager/StatisticActivity.java
+++ projekat/app/src/main/java/ra47_2014/pnrs1/rtrk/taskmanager/StatisticActivity.java
@@ -2,13 +2,83 @@ package ra47_2014.pnrs1.rtrk.taskmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class StatisticActivity extends AppCompatActivity {
+
+    Button mButton;
+    RadioButton rbLow;
+    RadioButton rbMed;
+    RadioButton rbHigh;
+    StatisticsView pieChart;
+    StatisticActivity thisActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        StatisticsView statisticsView = new StatisticsView(getApplicationContext());
-        setContentView(statisticsView);
+        setContentView(R.layout.activity_statistic);
+        thisActivity = this;
+
+        mButton = (Button) findViewById(R.id.buttonBack);
+        pieChart = (StatisticsView) findViewById(R.id.pieChart);
+        final RadioButton rbLow = (RadioButton) findViewById(R.id.rbLow);
+        final RadioButton rbMed = (RadioButton) findViewById(R.id.rbMedium);
+        final RadioButton rbHigh = (RadioButton) findViewById(R.id.rbHigh);
+
+        rbLow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbLow.setClickable(false);
+                rbMed.setClickable(true);
+                rbHigh.setClickable(true);
+                pieChart.init();
+                pieChart.setPercentage(13.f);
+                pieChart.setColor(R.color.greenButton);
+                pieChart.setBgdColor(R.color.greenButtonPressed);
+                pieChart.invalidate();
+            }
+        });
+
+        rbMed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbLow.setClickable(true);
+                rbMed.setClickable(false);
+                rbHigh.setClickable(true);
+                rbMed.toggle();
+                pieChart.init();
+                pieChart.setPercentage(73.f);
+                pieChart.invalidate();
+                pieChart.setColor(R.color.yellowButton);
+                pieChart.setBgdColor(R.color.yellowButtonPressed);
+                pieChart.invalidate();
+            }
+        });
+
+        rbHigh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbLow.setClickable(true);
+                rbMed.setClickable(true);
+                rbHigh.setClickable(false);
+                pieChart.init();
+                pieChart.setPercentage(49.f);
+                pieChart.setColor(R.color.redButton);
+                pieChart.setBgdColor(R.color.redButtonPressed);
+                pieChart.invalidate();
+            }
+        });
+        rbLow.setChecked(true);
+        rbLow.callOnClick();
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                thisActivity.finish();
+            }
+        });
     }
 }
--- projekat/app/src/main/java/ra47_2014/pnrs1/rtrk/taskmanager/StatisticsView.java
+++ projekat/app/src/main/java/ra47_2014/pnrs1/rtrk/taskmanager/StatisticsView.java
@@ -8,31 +8,95 @@ import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by student on 24.4.2017.
  */
 
-public class StatisticsView extends View {
+public class StatisticsView extends View{
 
     private Paint mPaint;
-    private Canvas mCanvas;
+    private Paint mBorderPaint;
+    private Paint mBackgroundPaint;
+    private Paint mTextPaint;
+    private RectF mRect;
+    private float mPercentage;
+    private float mDrawnPercentage;
 
     public StatisticsView(Context context) {
         super(context);
+        init();
     }
 
     public StatisticsView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public StatisticsView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
+    public void setPercentage(float percentage){
+        this.mPercentage = percentage;
+    }
+
+    public void redrawPercentage(){
+        mDrawnPercentage++;
+        invalidate();
+    }
+
+    public void setColor(int colorId){
+        mPaint.setColor(getContext().getResources().getColor(colorId));
+    }
+
+    public void setBgdColor(int colorId){
+        mBackgroundPaint.setColor(getContext().getResources().getColor(colorId));
+    }
+
+    public void init(){
+
+        mPercentage = 0;
+        mDrawnPercentage = 0;
+
+        mPaint = new Paint();
+        mPaint.setColor(getContext().getResources().getColor(R.color.greenButton));
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        mBorderPaint = new Paint();
+        mBorderPaint.setColor(getContext().getResources().getColor(R.color.colorAccent));
+        mBorderPaint.setAntiAlias(true);
+        mBorderPaint.setStyle(Paint.Style.STROKE);
+
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(getContext().getResources().getColor(R.color.yellowBgd));
+        mBackgroundPaint.setAntiAlias(true);
+        mBackgroundPaint.setStyle(Paint.Style.FILL);
+
+        mTextPaint = new Paint();
+        mTextPaint.setColor(getContext().getResources().getColor(R.color.colorPrimary));
+        mTextPaint.setTextSize(45);
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setStyle(Paint.Style.FILL);
+
+        mRect = new RectF();
+    }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        mPaint.setColor(Color.rgb(30,154,30));
-        mCanvas.drawArc(new RectF(getLeft(),getTop(),getWidth()/3,getHeight()/4),90,250,true,mPaint);
+
+        mRect.set(0, 0, getWidth(),getHeight());
+        canvas.drawArc(mRect, -90, 360, true, mBackgroundPaint);
+        canvas.drawArc(mRect, -90, 3.6f*mDrawnPercentage, true, mPaint);
+        canvas.drawArc(mRect, -90, 3.6f*mDrawnPercentage, true, mBorderPaint);
+        canvas.drawText(String.valueOf(mDrawnPercentage)+"%",getWidth()/2-45,getHeight()/2+20,mTextPaint);
+        if(mPercentage!=mDrawnPercentage)
+        {
+            redrawPercentage();
+        }
     }
 }
